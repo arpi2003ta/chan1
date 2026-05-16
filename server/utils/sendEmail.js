94d456906eac37f9b5d8f4502fecac5cfd04c1ef
arpi2003ta <145153525+arpi2003ta@users.noreply.github.com>
2026-05-17 00:32:54 +0530

    Configure nodemailer transport settings for email

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -37,6 +37,9 @@ dotenv.config();
 export const sendCertificateEmail = async (toEmail, certPath) => {
   const transporter = nodemailer.createTransport({
     service: "gmail",
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
     auth: {
       user: process.env.MAIL_ID,
       pass: process.env.MAIL_PASSWORD,
